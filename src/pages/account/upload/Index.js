f9234497996766d13cfb1b8ad8940fe212024adf
kawsersarker <s325902@students.cdu.edu.au>
2021-11-05 08:12:56 +0930

    recall function error, upload code redone

--- src/pages/account/upload/Index.js
+++ src/pages/account/upload/Index.js
@@ -1,10 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { Container } from '../../../components/container/Index'
-import { MultiFileUploader } from '../../../components/fileUploader/MultiFileUploader'
+import { FileUploader } from '../../../components/fileUploader/FileUploader'
 import { UploadProgressCard } from '../../../components/uploadProgressCard/UploadProgressCard'
+import { Requests } from '../../../utils/requests/Index'
 
 const Index = () => {
     const [selectedFiles, setSelectedFiles] = useState([])
+    const [categories, setCategories] = useState([])
+    const [tags, setTags] = useState([])
+
+    // fetch category
+    const fetchCategory = useCallback(async () => {
+        try {
+            const response = await Requests.Web.Category()
+            if (response && response.status === 200) {
+                if (response.data.data && response.data.data.length) {
+                    const items = []
+                    for (let i = 0; i < response.data.data.length; i++) {
+                        const element = response.data.data[i]
+                        items.push({
+                            label: element.name,
+                            value: element._id
+                        })
+                    }
+
+                    setCategories(items)
+                }
+            }
+        } catch (error) {
+            if (error) console.log(error)
+        }
+    }, [])
+
+    // fetch tags
+    const fetchTags = useCallback(async () => {
+        try {
+            const response = await Requests.Web.Tags()
+            if (response && response.status === 200) {
+                if (response.data.data && response.data.data.length) {
+                    const items = []
+                    for (let i = 0; i < response.data.data.length; i++) {
+                        const element = response.data.data[i]
+                        items.push(element.title)
+                    }
+
+                    setTags(items)
+                }
+            }
+        } catch (error) {
+            if (error) console.log(error)
+        }
+    }, [])
+
+
+    useEffect(() => {
+        fetchCategory()
+    }, [fetchCategory])
+
+    useEffect(() => {
+        fetchTags()
+    }, [fetchTags])
 
     // Handle files
     const handleFiles = data => {
@@ -15,13 +70,6 @@ const Index = () => {
         setSelectedFiles(filesArray)
     }
 
-    // Handle remove file
-    const handleRemove = data => {
-        const withOutThisFile = selectedFiles.filter(x => x.name !== data.name)
-        const newFiles = [...withOutThisFile]
-        setSelectedFiles(newFiles)
-    }
-
     return (
         <div className="file-uploader-container">
             <Container.Fluid>
@@ -30,8 +78,8 @@ const Index = () => {
                 {selectedFiles && !selectedFiles.length ?
                     <Container.Row className="py-30">
                         <Container.Column className="col-md-8 col-lg-7 col-xl-6 col-xxl-5 m-auto">
-                            <MultiFileUploader
-                                multi={true}
+                            <FileUploader
+                                multi={false}
                                 onSelected={files => handleFiles(files)}
                             />
                         </Container.Column>
@@ -48,7 +96,8 @@ const Index = () => {
                                 <UploadProgressCard
                                     key={i}
                                     data={item}
-                                    onClick={() => handleRemove(item)}
+                                    tags={tags}
+                                    categories={categories}
                                 />
                             )}
 
